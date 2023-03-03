9171b5859b0b6eda91b7cfb26161a1a91f0e7062
Karliny <karlinyphernandez@gmail.com>
2023-03-03 17:32:57 -0300

    exercícios

--- ex070.py
+++ ex070.py
@@ -29,15 +29,27 @@ print(f'{nomeproduto} foi o menor produto e custou {menor}.')
 
 
 '''Resposta professor:
-total = 0
+total = totmil = menor = cont = barato = 0
 while True:
     produto = str(input('Qual o nome do produto? '))
-    preço = float(input('Quanto custa o produto? '))
+    preço = float(input('Quanto custa o produto? R$ '))
+    cont = cont + 1
     total = total + preço
     resp = ' '
-    
+    if preço > 1000:
+        totmil = totmil + 1
+    if cont == 1:
+        menor = preço
+        barato = produto
+    else:
+        if preço < menor:
+            menor = preço
+            barato = produto
     while resp not in 'SN':
         resp = str(input('Quer continuar? S/N ')).strip().upper()[0]
     if resp == 'N':
         break
+print(f'O total da compra foi de R${total} Reais')
+print(f'Temos {totmil} produtos custando mais de 1000 Reais')
+print(f'O menor produto foi {barato} e custou R${menor} ')
     print('Fim do programa!')'''
--- ex071.py
+++ ex071.py
@@ -1,5 +1,34 @@
 #Crie um programa que simule um caixa eletronico. Pergunte ao usuario qual valor a ser sacadao (inteiro), o programa vai informar quantas cédulas de cada valor serao entregues, considerar que o caixa possui cedulas de 50, 20, 10 e 1 
 
+#Modo correto do professor:
+print('=' * 30)
+print('{:^30}' .format('BANCO KAH'))
+print('=' * 30)
+valor = int(input('Qual valor você quer sacar? R$ '))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total = total - ced
+        totced = totced + 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Fim do programa')
+print('=' * 30)
+
+
 #modo que eu criei:
 num = 0
 u = num // 1 % 10
